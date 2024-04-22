517fd5e9dcd473962c64b496a09dc9feb8707996
nafasov <nafasovnurxon18@gmail.com>
2024-04-22 21:18:10 +0500

    modidied

--- config/urls.py
+++ config/urls.py
@@ -26,3 +26,4 @@ urlpatterns = [
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
