e31bd66beb022afa26c99860813575049abca603
nafasov <nafasovnurxon18@gmail.com>
2024-05-04 17:58:50 +0500

    modified

--- apps/order/admin.py
+++ apps/order/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    CartItem,
+    Order,
+    Promo
+    )
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'quantity', 'unit_price')
+    list_filter = ('quantity', )
+    search_fields = ('user', )
+    readonly_fields = ('modified_date', 'created_date')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'is_delivered', 'created_date', 'modified_date')
+    date_hierarchy = 'created_date'
+    search_fields = ('user', )
+    readonly_fields = ('modified_date', 'created_date')
+
+
+@admin.register(Promo)
+class PromoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'discount', 'min_price', 'created_date')
+    date_hierarchy = 'created_date'
+    search_fields = ('user', )
+    readonly_fields = ('created_date',)
+    list_filter = ('discount',)
+
--- apps/order/models.py
+++ apps/order/models.py
@@ -1,2 +1,53 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+from apps.account.models import User
+from apps.product.models import Product
+
+
+class Promo(models.Model):
+    name = models.CharField(max_length=8)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+    expire_date = models.DateField(null=True, blank=True)
+    discount = models.PositiveIntegerField(validators=[MaxValueValidator(100)])
+    min_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(250000.00)])
+    is_expired = models.BooleanField(default=False)
+    members = models.ManyToManyField(User, related_name='promos', blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    is_ordered = models.BooleanField(default=False)
+    modified_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
+
+    @property
+    def get_amount(self):
+        return self.unit_price * self.quantity
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(CartItem)
+    promo = models.CharField(max_length=8, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    is_delivered = models.BooleanField(default=False)
+    modified_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def get_amount(self):
+        return sum(self.items.values_list('get_amount', flat=True))
 
