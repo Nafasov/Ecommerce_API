0ae606f4fa1d894f4a714f14e2ad367d0de995ac
nafasov <nafasovnurxon18@gmail.com>
2024-05-16 17:32:29 +0500

    modified

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,1 +1,23 @@
 from django.urls import path
+
+
+'''
+    Register:
+        - /register/ -> send code to user via mail
+        - /verify/email/ -> verify code -> activate, give token
+    
+    Login:
+        - /login/ -> give token 
+        
+    Verify Account:
+        - /send/email/ -> send code to user via mail
+        - /verify/email/ -> verify code -> activate, give token
+        
+    Change Password:
+        - /change/password/ -> change password
+    
+    Reset Password:
+            - /register/ -> send code to user via mail
+            - /verify/email/ -> verify code -> activate, give token
+        - /reset/password/ -> reset password
+'''
